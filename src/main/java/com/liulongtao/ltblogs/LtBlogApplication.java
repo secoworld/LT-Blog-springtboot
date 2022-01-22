af049f73310ef34bc335578dafd596d4a85e3d5c
longtao <longtao55@163.com>
2022-01-22 22:56:45 +0800

    create lt blog springboot application

---
+++ src/main/java/com/liulongtao/ltblogs/LtBlogApplication.java
@@ -0,0 +1,13 @@
+package com.liulongtao.ltblogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LtBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LtBlogApplication.class, args);
+    }
+
+}
